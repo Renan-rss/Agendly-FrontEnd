8fd268d9cbed36d5feb2440a53798fbb6e0189e4
Kevyn Brian <kevyn.brian9@gmail.com>
2025-12-12 21:55:45 -0300

    Atualização tela de login e Home de Administrador/ Criação tela para redefinir senha

--- agendly/src/App.jsx
+++ agendly/src/App.jsx
@@ -1,16 +1,33 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import CadastroUsuario from "./pages/CadastroUsuario/CadastroUsuario";
+import ResetSenha from "./pages/ResetSenha/ResetSenha";
 import HomeAdmin from "./pages/Home/Administrador/HomeAdmin";
+import AdminDashboard from "./pages/Home/Administrador/AdminDashboard";
+import CadastroProfissional from "./pages/Home/Administrador/HomeAdmin";
+import Teste from "./pages/Home/Administrador/Teste";
+
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <Routes>
+
         <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<CadastroUsuario />} />
-        <Route path="/admin" element={<HomeAdmin />} />
+        <Route path="/reset-senha" element={<ResetSenha />} />
+
+        <Route path="/admin" element={<HomeAdmin />}>
+          <Route index element={<AdminDashboard />} />
+          <Route path="teste" element={<Teste />} />
+          <Route
+            path="cadastrar-profissional"
+            element={<CadastroProfissional />}
+          />
+        </Route>
+
       </Routes>
     </div>
   );
---
+++ agendly/src/pages/Home/Administrador/Teste.jsx
@@ -0,0 +1,8 @@
+export default function Teste() {
+  return (
+    <div>
+      <h1>Conteúdo do Admin</h1>
+      <p>Container Direito</p>
+    </div>
+  );
+}
